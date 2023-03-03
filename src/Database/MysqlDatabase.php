27af3662da20a13ed34712c10410b4ddeedf2a64
Fyronix <86478645+Fyronix@users.noreply.github.com>
2023-03-03 14:44:37 +0100

    Update MysqlDatabase.php

--- src/Database/MysqlDatabase.php
+++ src/Database/MysqlDatabase.php
@@ -1,16 +1,22 @@
 <?php
+
 namespace NeeZiaa\Database;
 
 use PDO;
 
-class MysqlDatabase extends Database {
-
+class MysqlDatabase extends Database 
+{
     public function getPDO($host, $db, $user, $password): PDO
     {
-        return (new PDO('mysql:dbname=' . $db . ';host=' . $host, $user, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        ]));
+        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
+        $pdoOptions = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4', SESSION sql_mode = 'STRICT_ALL_TABLES'"
+        ];
+
+        return new PDO($dsn, $user, $password, $pdoOptions);
     }
-
 }
