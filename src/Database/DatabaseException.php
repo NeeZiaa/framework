6939fb39166b66a4c4fb3d8905ab80320420d46f
Fyronix <86478645+Fyronix@users.noreply.github.com>
2023-03-03 14:51:44 +0100

    Update DatabaseException.php

--- src/Database/DatabaseException.php
+++ src/Database/DatabaseException.php
@@ -1,7 +1,15 @@
-<?php
 namespace NeeZiaa\Database;
 
-class DatabaseException extends \Exception
+abstract class Database
 {
-
+    public static function create(string $type, array $config): ?DatabaseInterface
+    {
+        switch ($type) {
+            case 'mysql':
+                return new MysqlDatabase($config['host'], $config['database'], $config['username'], $config['password']);
+            // Add support for other database types here
+            default:
+                throw new DatabaseException('Unsupported database type: ' . $type);
+        }
+    }
 }
