5235f80c2dee2d72340bf40ddd01c928de732128
Fyronix <86478645+Fyronix@users.noreply.github.com>
2023-03-03 14:47:02 +0100

    Update Database.php

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -2,7 +2,21 @@
 
 namespace NeeZiaa\Database;
 
-class Database
-{
+use PDO;
 
+class MysqlDatabase extends Database 
+{
+    public function getPDO($host, $db, $user, $password): PDO
+    {
+        $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+        $pdoOptions = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4', SESSION sql_mode = 'STRICT_ALL_TABLES'"
+        ];
+
+        return new PDO($dsn, $user, $password, $pdoOptions);
+    }
 }
